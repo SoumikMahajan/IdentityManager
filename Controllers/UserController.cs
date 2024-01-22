cd7d4708531adf71406e0acce3fb4cbce80e99b2
SoumikMahajan <soumik800@gmail.com>
2024-01-22 15:46:41 +0530

    Assign role to user

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,5 +1,6 @@
 ﻿using IdentityManager.Data;
 using IdentityManager.Models;
+using IdentityManager.Models.ViewModels;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
@@ -39,6 +40,111 @@ namespace IdentityManager.Controllers
             }
 
             return View(userList);
+        }
+
+        public async Task<IActionResult> ManageRole(string userId)
+        {
+            ApplicationUser user = await _userManager.FindByIdAsync(userId);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            List<string> exsitingUserRoles = await _userManager.GetRolesAsync(user) as List<string>;
+            var model = new RolesViewModel()
+            {
+                User = user
+            };
+
+            foreach (var role in _roleManager.Roles)
+            {
+                RoleSelection roleSelection = new()
+                {
+                    RoleName = role.Name
+                };
+                if (exsitingUserRoles.Any(c => c == role.Name))
+                {
+                    roleSelection.IsSelected = true;
+                }
+                model.RolesList.Add(roleSelection);
+            }
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> ManageRole(RolesViewModel rolesViewModel)
+        {
+            ApplicationUser user = await _userManager.FindByIdAsync(rolesViewModel.User.Id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            var oldUserRoles = await _userManager.GetRolesAsync(user);
+            var result = await _userManager.RemoveFromRolesAsync(user, oldUserRoles);
+
+            if (!result.Succeeded)
+            {
+                TempData[SD.Error] = "Error while removing roles";
+                return View(rolesViewModel);
+            }
+
+            result = await _userManager.AddToRolesAsync(user,
+                rolesViewModel.RolesList.Where(x => x.IsSelected).Select(y => y.RoleName));
+
+            if (!result.Succeeded)
+            {
+                TempData[SD.Error] = "Error while adding roles";
+                return View(rolesViewModel);
+            }
+
+            TempData[SD.Success] = "Roles assigned successfully";
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> LockUnlock(string userId)
+        {
+            ApplicationUser user = _db.ApplicationUser.FirstOrDefault(u => u.Id == userId);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            if (user.LockoutEnd != null && user.LockoutEnd > DateTime.Now)
+            {
+                //user is locked and will remain locked untill lockoutend time
+                //clicking on this action will unlock them
+                user.LockoutEnd = DateTime.Now;
+                TempData[SD.Success] = "User unlocked successfully";
+            }
+            else
+            {
+                //user is not locked, and we want to lock the user
+                user.LockoutEnd = DateTime.Now.AddYears(1000);
+                TempData[SD.Success] = "User locked successfully";
+            }
+            _db.SaveChanges();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteUser(string userId)
+        {
+            var user = _db.ApplicationUser.FirstOrDefault(u => u.Id == userId);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            _db.ApplicationUser.Remove(user);
+            _db.SaveChanges();
+            TempData[SD.Success] = "User deleted successfully";
+            return RedirectToAction(nameof(Index));
+
         }
     }
 }
